5bdd39a31fd1063a2376b966d6e3e3cbad83b22b
Tanner Vanlangeveld <t.vanlangeveld@outlook.com>
2023-02-06 10:14:40 -0700

    final commit

--- addToZero.js
+++ addToZero.js
@@ -2,3 +2,31 @@
 let array = [28, 43, -12, 30, 4, 0, 12]
 
 // Write your solution below:
+
+
+// for(let i = 0; i < array.length; i++){
+//     if(array[i] + array[i] === 0)
+//     console.log(true) 
+//        else {
+//             console.log(false)
+//         }
+//     }
+
+// for(let i = 0; i < array.length; i++){
+//     if(array[i] + array[i])
+ 
+//     console.log(i + i)
+// }
+
+
+
+let value = true
+for (let i = 0; i < array.length; i++){
+    for (let j = 0; j < array.length; i++){
+        if(i !== j){
+            if( array[i] + array[j] === 0) {
+              console.log(array)
+            }
+        }
+    }
+}  
